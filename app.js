8701f574b7d1dc280605f68a72b85f0f59fbb25b
9656pawankumar <114625079+9656pawankumar@users.noreply.github.com>
2023-09-16 13:13:35 -0400

    added create camp page

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 const campground = require('./models/campground');
-
+const methodOverride = require('method-override');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     
 });
@@ -14,6 +14,8 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 
 app.set('view engine','ejs');
@@ -28,10 +30,25 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds })
 });
 
+app.post('/campgrounds', async (req, res) => {
+    const campground1 = new campground(req.body.campground);
+    await campground1.save();
+    res.redirect(`/campgrounds/${campground1._id}`)
+})
+
+
+app.get('/campgrounds/new',async(req,res)=>{
+    res.render('campgrounds/new');
+});
+
 app.get('/campgrounds/:id', async (req, res,) => {
     const campground1 = await campground.findById(req.params.id);
     res.render('campgrounds/show', { campground1 });
 });
+
+
+
+
 
 app.listen(3000,()=>{
     console.log('connected to port 3000')
